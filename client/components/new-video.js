78e2fc0b8db3e4ef097f5835781f897a1278bd5d
Qi Lin <qlin1996@gmail.com>
2020-08-16 14:56:35 -0400

    updated for 1 to 1 calls only

--- client/components/new-video.js
+++ client/components/new-video.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import {connect} from 'react-redux'
+import {Link} from 'react-router-dom'
 import socket from '../socket'
 // peer is a WebTRC API that creates a userId and allows us to connect to other users
 import Peer from 'peerjs'
@@ -8,49 +9,36 @@ class NewVideo extends React.Component {
   constructor() {
     super()
     this.state = {
-      peers: {},
-      videoGrid: ''
+      otherUsersConnection: 'waiting'
     }
   }
-
   async componentDidMount() {
-    const videoGrid = document.querySelector('#video-grid')
-    this.setState(prevState => ({
-      peers: prevState.peers,
-      videoGrid: videoGrid
-    }))
-
     const myPeer = new Peer()
     myPeer.on('open', myUserId => {
       // 1. send to server (roomId and userId) to let others know youve joined room
       socket.emit('join-room', this.props.match.params.roomId, myUserId)
     })
-
-    // ask for access to webcam and audio
+    // allow access to webcam and audio
     const myStream = await window.navigator.mediaDevices.getUserMedia({
       video: true,
       audio: true
     })
     // once stream is available, set it as the source object for the video tag and start video
-    const myVideo = document.createElement('video')
-    myVideo.muted = true
-    myVideo.srcObject = myStream
-    myVideo.addEventListener('loadedmetadata', () => {
-      myVideo.play()
+    this.myVideo.muted = true
+    this.myVideo.srcObject = myStream
+    this.myVideo.addEventListener('loadedmetadata', () => {
+      this.myVideo.play()
     })
-    this.state.videoGrid.append(myVideo)
-
-    // 7. when new user tries to calls us, answer and send them our stream
+    // 7. when new user tries to call us, answer and send them our stream
     myPeer.on('call', call => {
       call.answer(myStream)
-      // and add to their page
-      const othersVideo = document.createElement('video')
       call.on('stream', newUserStream => {
-        othersVideo.srcObject = newUserStream
-        othersVideo.addEventListener('loadedmetadata', () => {
-          othersVideo.play()
+        this.remoteVideo.muted = true
+        this.remoteVideo.srcObject = newUserStream
+        this.remoteVideo.addEventListener('loadedmetadata', () => {
+          this.remoteVideo.play()
         })
-        this.state.videoGrid.append(othersVideo)
+        this.setState({otherUsersConnection: 'connected'})
       })
     })
     // 4. listens for user-connected (userId) from server
@@ -58,35 +46,44 @@ class NewVideo extends React.Component {
       // 5. use peer to call new users so users connect directly with each other now. send new user your stream.
       const call = myPeer.call(newUserId, myStream)
       // 6. when new user sends back their stream, add it to our page
-      const othersVideo = document.createElement('video')
       call.on('stream', newUserStream => {
-        othersVideo.srcObject = newUserStream
-        othersVideo.addEventListener('loadedmetadata', () => {
-          othersVideo.play()
+        this.remoteVideo.srcObject = newUserStream
+        this.remoteVideo.addEventListener('loadedmetadata', () => {
+          this.remoteVideo.play()
         })
-        this.state.videoGrid.append(othersVideo)
+        this.setState({otherUsersConnection: 'connected'})
       })
-      // when new user leaves, remove video
-      call.on('close', () => {
-        othersVideo.remove()
-      })
-      // store this call in state to keep track of all calls
-      this.setState(prevState => ({
-        peers: {...prevState.peers, [newUserId]: call},
-        videoGrid: prevState.videoGrid
-      }))
     })
-
     // 8. when new user leaves, update state and let server know
-    socket.on('user-disconnected', newUserId => {
-      if (this.state.peers[newUserId]) this.state.peers[newUserId].close()
+    socket.on('user-disconnected', () => {
+      this.remoteVideo.remove()
+      this.setState({otherUsersConnection: 'left'})
     })
   }
-
   render() {
     return (
-      <div>
-        <div id="video-grid" />
+      <div id="video-grid">
+        {this.state.otherUsersConnection === 'waiting' && (
+          <p>Waiting for other user to connect</p>
+        )}
+        <video
+          ref={video => {
+            this.myVideo = video
+          }}
+        />
+        <video
+          ref={video => {
+            this.remoteVideo = video
+          }}
+        />
+        {this.state.otherUsersConnection === 'left' && (
+          <div>
+            <p>The other user has hung up</p>
+            <Link to="/">
+              <button type="button">Return to home</button>
+            </Link>
+          </div>
+        )}
       </div>
     )
   }
