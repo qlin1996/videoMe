1e080a69faa5b0940d80f7224652139e2c2d5326
Qi Lin <qlin1996@gmail.com>
2020-08-17 22:33:08 -0400

    added socket transport

--- client/socket.js
+++ client/socket.js
@@ -1,6 +1,6 @@
 import io from 'socket.io-client'
 
-const socket = io(window.location.origin)
+const socket = io(window.location.origin, {transports: ['polling']})
 
 socket.on('connect', () => {
   console.log('Connected!')
